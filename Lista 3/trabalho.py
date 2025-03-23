1697115436c6041a2f137b527d5748b59fdad3b5
Fabio Cordeiro <fabiojp1997@hotmail.com>
2025-03-22 23:19:45 -0300

    terceiro exercicio

--- Lista 3/trabalho.py
+++ Lista 3/trabalho.py
@@ -1,8 +1,21 @@
-# Exercício 01
-nome = input("Olá caro usuário, por favor digite seu nome...")
-print( f"Seja bem vindo {nome} a nossa plataforma!")
+# # Exercício 01
+# nome = input("Olá caro usuário, por favor digite seu nome...")
+# print( f"Seja bem vindo {nome} a nossa plataforma!")
 
-# Exercício 02
-dec_01 = float(input("Digite dois números com casa decimal: "))
-dec_02 = float(input("Digite o segundo número: "))
-print( f"A soma de {dec_01} e {dec_02} é  {dec_01+dec_02:.2f}")
+# # Exercício 02
+# dec_01 = float(input("Digite dois números com casa decimal: "))
+# dec_02 = float(input("Digite o segundo número: "))
+# print( f"A soma de {dec_01} e {dec_02} é  {dec_01+dec_02:.2f}")
+
+# Exercicio 03
+int_01 = int(input("Digite o primeiro numero: "))
+int_02 = int(input("Digite o segundo numero: "))
+operador = input("Digite o tipo de operação: ")
+if(operador == "+"):
+    print( f"O resultado de {int_01}{operador}{int_02} é {int_01+int_02}")
+elif(operador == "-"):
+    print( f"O resultado de {int_01}{operador}{int_02} é {int_01-int_02}")
+elif(operador == "/"):
+    print( f"O resultado de {int_01}{operador}{int_02} é {int_01//int_02}")
+elif(operador == "*"):
+    print( f"O resultado de {int_01}{operador}{int_02} é {int_01*int_02}")
