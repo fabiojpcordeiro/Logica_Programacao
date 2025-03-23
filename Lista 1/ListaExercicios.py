44e0ed4ea1bd6151902bbb4ea7a48ae392de0820
Fabio Cordeiro <fabiojp1997@hotmail.com>
2025-03-22 22:55:25 -0300

    Primeiro push

---
+++ Lista 1/ListaExercicios.py
@@ -0,0 +1,59 @@
+# Exercicio 1
+
+idade = 20
+print(idade)
+
+#Exercicio 2
+nome = "Fabio"
+sobrenome = "Cordeiro"
+print(nome + " " + sobrenome)
+
+#Exercicio 3
+numero = 10
+if numero > 5:
+    print("O numero é maior que 5")
+else:
+    print("O número é menor que 5")
+
+#Exercicio 4 
+peso = 70.5
+print("O peso é: ",peso)
+
+#Exercicio 5
+cidade = "Guarapuava"
+print(cidade)
+
+#Exercicio 6
+numero2 = -38
+if numero2 > 0:
+    print(f"O numero {numero2} é positivo")
+else:
+    print(f"O numero {numero2} é negativo")
+
+#Exercicio 7
+idade2 = 22
+if idade2 >= 16:
+    print("Você já pode votar")
+else:
+    print("Você ainda não pode votar")
+
+#Exercicio 8
+idade3 = 15
+if idade3 >= 18:
+    print("Maior de idade")
+else:
+    print("Menor de idade")
+
+#Exercicio 9
+nota = 7
+if nota >= 5:
+    print("Aprovado")
+else:
+    print("Reprovado")
+
+#Exercicio 10
+senha = "12345"
+if senha == "12345":
+    print("Acesso permitido")
+else:
+    print("Acesso negado")
---
+++ Lista 3/trabalho.py
@@ -0,0 +1,3 @@
+# Exercício 01
+nome = input("Olá caro usuário, por favor digite seu nome...")
+print( f"Seja bem vindo {nome} a nossa plataforma!")
