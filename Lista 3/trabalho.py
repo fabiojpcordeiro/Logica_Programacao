7cd8ce6200574c05b079b5ca082089ea642e796d
Fabio Cordeiro <fabiojp1997@hotmail.com>
2025-03-22 23:06:25 -0300

    Segundo Exercicio

--- Lista 3/trabalho.py
+++ Lista 3/trabalho.py
@@ -1,3 +1,8 @@
 # Exercício 01
 nome = input("Olá caro usuário, por favor digite seu nome...")
 print( f"Seja bem vindo {nome} a nossa plataforma!")
+
+# Exercício 02
+dec_01 = float(input("Digite dois números com casa decimal: "))
+dec_02 = float(input("Digite o segundo número: "))
+print( f"A soma de {dec_01} e {dec_02} é  {dec_01+dec_02:.2f}")
