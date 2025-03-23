f6f9108fb56bfe3a13234af97d3ad47fb23a43e0
Fabio Cordeiro <fabiojp1997@hotmail.com>
2025-03-22 23:32:01 -0300

    exercicio 04

--- Lista 3/trabalho.py
+++ Lista 3/trabalho.py
@@ -7,15 +7,20 @@
 # dec_02 = float(input("Digite o segundo número: "))
 # print( f"A soma de {dec_01} e {dec_02} é  {dec_01+dec_02:.2f}")
 
-# Exercicio 03
-int_01 = int(input("Digite o primeiro numero: "))
-int_02 = int(input("Digite o segundo numero: "))
-operador = input("Digite o tipo de operação: ")
-if(operador == "+"):
-    print( f"O resultado de {int_01}{operador}{int_02} é {int_01+int_02}")
-elif(operador == "-"):
-    print( f"O resultado de {int_01}{operador}{int_02} é {int_01-int_02}")
-elif(operador == "/"):
-    print( f"O resultado de {int_01}{operador}{int_02} é {int_01//int_02}")
-elif(operador == "*"):
-    print( f"O resultado de {int_01}{operador}{int_02} é {int_01*int_02}")
+# # Exercicio 03
+# int_01 = int(input("Digite o primeiro numero: "))
+# int_02 = int(input("Digite o segundo numero: "))
+# operador = input("Digite o tipo de operação: ")
+# if(operador == "+"):
+#     print( f"O resultado de {int_01}{operador}{int_02} é {int_01+int_02}")
+# elif(operador == "-"):
+#     print( f"O resultado de {int_01}{operador}{int_02} é {int_01-int_02}")
+# elif(operador == "/"):
+#     print( f"O resultado de {int_01}{operador}{int_02} é {int_01//int_02}")
+# elif(operador == "*"):
+#     print( f"O resultado de {int_01}{operador}{int_02} é {int_01*int_02}")
+
+# Exercicio 04
+# Excluindo anos bissextos e assumindo 1 mês = 30 dias.
+idade_ex04 = int(input("Caso usuário, digite sua idade em anos..."))
+print( f"Você tem {idade_ex04 * 12} meses e {idade_ex04 * 365} dias de vida!!!")
